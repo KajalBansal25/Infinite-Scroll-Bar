a9feb511462257479d27ef00642d0fe0c344862c
KajalBansal25 <kajal.bansal@daffodilsw.com>
2022-05-16 23:55:29 +0530

    infinite scroll bar

---
+++ scrolling_demo/App.js
@@ -0,0 +1,86 @@
+import React, {useEffect, useState} from 'react';
+import {
+  FlatList,
+  Text,
+  View,
+  ActivityIndicator,
+  StyleSheet,
+} from 'react-native';
+
+import axios from 'axios';
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const getUsers = () => {
+    axios({
+      method: 'get',
+      url: 'https://gorest.co.in/public/v2/todos',
+    }).then(function (response) {
+      setUsers([...users,response.data]);
+      console.log(">>>>>>>>>>>",users)
+    });
+  };
+  const renderItem = ({item,index}) => {
+    if (index > 19) {
+      index = (index % 10);
+    }
+    return (
+      <View>
+        <View style={styles.containerContent}>
+          <Text>User_id: {`${item[index].user_id}`}</Text>
+          <Text>title: {item[index].title}</Text>
+          <Text>Due_on: {item[index].due_on}</Text>
+          <Text>Status: {item[index].status}</Text>
+          {/* {console.log("item>>",item[index])} */}
+        </View>
+      </View>
+    );
+  };
+
+  const renderLoader = () => {
+    return (
+      <View style={StyleSheet.loaderStyle}>
+        <ActivityIndicator size="large" color="#aaa" />
+      </View>
+    );
+  };
+
+  const loadMoreItem = () => {
+    console.log('load more items');
+    getUsers();
+  
+  };
+
+  useEffect(() => {
+    getUsers();
+  },[]);
+
+  return (
+    <FlatList
+      data={users}
+      renderItem={renderItem}
+      keyExtractor = {(item, index) => index } 
+      ListFooterComponent={renderLoader}
+      onEndReached={loadMoreItem}
+      onEndReachedThreshold={0.5}
+    />
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  loaderStyle: {
+    marginVertical: 16,
+    alignItems: 'center',
+  },
+  containerContent: {
+    borderBottomColor: '#aaa',
+    borderTopWidth: 0,
+    borderWidth: 1,
+    marginTop: 20,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
